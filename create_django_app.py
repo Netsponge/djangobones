60a7eaf070bad279648a1c954b425e65ccd81b41
jean baptiste marrec <jeanbaptiste.marrec@gmail.com>
2024-11-04 16:42:32 +0100

    adding folder venv, project, and pip install

--- create_django_app.py
+++ create_django_app.py
@@ -1,19 +1,24 @@
-# -*- coding: utf-8 -*-
 import os
-import subprocess
 import sys
+import subprocess
 
-# Create a new folder "myapp"
-os.makedirs("myapp", exist_ok=True)
+# Définir le nom du projet Django et de l'application
+yourproject = "my_django_project"  # Remplacez par le nom souhaité pour votre projet
+app_name = "myapp"  # Nom de l'application Django
+python_executable = sys.executable
 
-# Create a virtual environment in "myapp/.venv"
-subprocess.run([sys.executable, "-m", "venv", "myapp/.venv"])
+# Chemin du dossier de l'environnement virtuel
+venv_path = ".venv"
 
-# Activate the virtual environment
-if os.name == 'nt':  # For Windows
-    activation_cmd = ["cmd", "/c", "myapp\\.venv\\Scripts\\activate.bat"]
-else:  # For Unix or macOS
-    activation_cmd = ["source", "myapp/.venv/bin/activate"]
+# Créer un environnement virtuel pour Python
+subprocess.run([python_executable, "-m", "venv", venv_path])
 
-subprocess.run(activation_cmd, shell=True)
+# Chemin vers le pip de l'environnement virtuel
+pip_path = os.path.join(venv_path, "Scripts", "pip") if os.name == 'nt' else os.path.join(venv_path, "bin", "pip")
+django_admin_path = os.path.join(venv_path, "Scripts", "django-admin") if os.name == 'nt' else os.path.join(venv_path, "bin", "django-admin")
 
+# Installer Django et d'autres packages nécessaires
+subprocess.run([pip_path, "install", "django", "djangorestframework", "django-environ"])
+
+# Créer le projet Django
+subprocess.run([django_admin_path, "startproject", yourproject])
